1bea43c7331c6f2a00c74b9fa6ef2d8bc562c83f
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-17 12:05:04 -0400

    typos

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-git add -const path = require('path');
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
