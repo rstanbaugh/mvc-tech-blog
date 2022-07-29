643777dbed36b2338159148d9e33ebbad107cf6d
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-29 18:23:31 -0400

    added new posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -81,7 +81,7 @@ router.post('/', withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
-    post_url: req.body.post_url,
+    post_content: req.body.post_content,
     user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
